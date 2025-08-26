9c16ade123bfaea8c1414f7bf0fc932f3d17f963
Marcin Siniarski <15676482+siniarskimar@users.noreply.github.com>
2025-08-26 21:06:09 +0200

    test bpm.saveSurface in an example

--- examples/shapes/main.zig
+++ examples/shapes/main.zig
@@ -1,4 +1,34 @@
 const std = @import("std");
 const zoftr = @import("zoftr");
+const bmp = zoftr.bmp;
 
-pub fn main() void {}
+pub fn main() !void {
+    var data: [256 * 256 * 3]u8 = undefined;
+
+    const checkerboard_bmp = try std.fs.cwd().createFile("checkerboard.bmp", .{});
+    defer checkerboard_bmp.close();
+
+    var buff_writer = std.io.bufferedWriter(checkerboard_bmp.writer());
+    defer buff_writer.flush() catch |err| {
+        std.log.err("failed to flush BMP stream: {}", .{err});
+    };
+
+    const surface = zoftr.Surface{
+        .data = data[0..],
+        .width = 32,
+        .height = 32,
+        .format = .bgr8,
+    };
+
+    const rect_size: usize = 8;
+
+    for (0..surface.height) |y| {
+        for (0..surface.width) |x| {
+            const odd = (y / rect_size + x / rect_size) % 2 == 0;
+            const pixel = surface.sample(@intCast(x), @intCast(y));
+            pixel[0..3].* = @splat(if (odd) 255 else 128);
+        }
+    }
+
+    try bmp.saveSurface(buff_writer.writer().any(), &surface);
+}
